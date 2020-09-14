8aa2a9654d0cd30aadb7c6acbbd9f6db99ed5bea
Justo Enrique Mansilla <jmansilla@udesa.edu.ar>
2020-09-14 13:53:42 -0300

    etructura controllers

---
+++ proyecto_integrador/controllers/usuarioController.js
@@ -0,0 +1,11 @@
+let usuarioController = {
+    regsitro: function (req, res) {
+        
+    },
+    agregarPost: function (req, res) {
+        
+    },
+    miPerfil: function (req, res) {
+        
+    },
+}
