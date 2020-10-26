cc0ad66b19be79dad825ae09014c6a6f1545f212
lucasromero42 <63105849+lucasromero42@users.noreply.github.com>
2020-10-26 12:56:20 -0300

    Login, cookies

--- controllers/miPerfilController.js
+++ controllers/miPerfilController.js
@@ -1,5 +1,8 @@
 let db = require('../database/models');
 let sequelize = db.sequelize;
+let bcrypt = require("bcryptjs");
+
+
 
 let miPerfilController = {
     miPerfil: function(req, res) {
@@ -8,12 +11,86 @@ let miPerfilController = {
 
     
     regsitracion: function (req, res) {
-    res.render('registracion',{title: 'registracion'})  
-    },
-    login: function (req, res) {
-    res.render('login',{title: 'login'})    
-    },
+      if (req.session.usuarioLogueado != undefined) {
+        res.redirect("/home");
+    }
 
-}
+    res.render("registracion");  
+    },
+    storeUser: function(req, res) {
+      if (req.session.usuarioLogueado != undefined) {
+          res.redirect("/home");
+      }
+
+      let name = req.body.name;
+      let password = bcrypt.hashSync(req.body.password, 10);
+      let email = req.body.email;
+
+      let user = {
+          name: name,
+          password: password,
+          email: email
+      }
+
+      db.User.create(user)
+      .then(function() {
+          res.redirect("/home");
+      })
+  },
+    login: function(req, res) {
+      if (req.session.usuarioLogueado != undefined) {
+          res.redirect("/home");
+      }
+
+      res.render("login");
+  },
+        processLogin: function(req, res) {
+          console.log(req.body);
+          if (req.session.usuarioLogueado != undefined) {
+              res.redirect("/home");
+          }
+          // Caso 1: El mail no esta en la base de datos y yo voy a tener que decirle al usuario: NO EXISTE
+          // Caso 2: El mail si existe pero la contraseña esta mal. Le tengo que decir al usuario: Usuario invalido
+          // Caso 3: Bienvenido!
+
+          // Casos que no voy a hacer: Dejaste los campos vacios. El mail no es un mail directamente.
+
+
+          // findAll retorna SIEMPRE un array. Si no matchean los datos findAll traer un array vacío pero SIEMPRE trae un array
+          // findOne en cambio tiene dos opciones. O trae el dato, o trae null.
+          db.User.findOne(
+              {
+                  where: [
+                      { email: req.body.email },
+                  ]
+              }
+          )
+          .then(function(usuario) {
+              if (usuario == null) {
+                  res.send("El mail no existe")
+              } else if (bcrypt.compareSync(req.body.password, usuario.password) == false) {
+                  res.send("Mala contraseña")
+              } else {
+                  req.session.usuarioLogueado = usuario;
+
+                  if (req.body.remember != undefined) {
+                      res.cookie("idDelUsuarioLogueado", usuario.id, {maxAge : 1000 * 20});
+                  }
+
+                  res.redirect("/home");
+                  // Todo bien!
+              }
+          })
+
+          
+      },
+
+      logout: function(req,res) {
+          req.session.usuarioLogueado = undefined;
+
+          res.redirect("/home");
+      }
+
+  }
 
 module.exports = miPerfilController;
--- routes/miPerfil.js
+++ routes/miPerfil.js
@@ -3,12 +3,15 @@ const { response } = require('../app');
 var router = express.Router();
 const miPerfilController = require("../controllers/miPerfilController")
 /* GET miperfil page. */
-//localhost:3000/miPerfil
-router.get('/',miPerfilController.miPerfil);
-//localhost:3000/miPerfil/registracion
-router.get('/registracion',miPerfilController.regsitracion);
-//localhost:3000/miPerfil/login
-router.get('/login',miPerfilController.login)
+router.get("/registracion", miPerfilController.regsitracion);
+
+router.post("/registracion", miPerfilController.storeUser);
+
+router.get("/login", miPerfilController.login);
+
+router.post("/login", miPerfilController.processLogin);
+
+router.get("/logout", miPerfilController.logout);
 
 
 
